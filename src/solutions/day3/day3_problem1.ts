e05ab3b7849ee70b031c94a1e966a4208569131a
Luiz Rocco <luizgustavorocco@gmail.com>
2022-12-06 00:46:38 -0300

    Day 3 problem 1 initial try

--- src/solutions/day3/day3_problem1.ts
+++ src/solutions/day3/day3_problem1.ts
@@ -5,14 +5,25 @@ export const day3_problem1 = () => {
   try {
     const problemPath = path.join(__dirname, "problem1_input.txt");
     const data: string = fs.readFileSync(problemPath, "utf8");
-    const answer = data.split("\n");
-    // .map(line => line.split(" "))
-    // .map(
-    //   ([opponentMove, myMove]) =>
-    //     calculatePoints(opponentMove as Move1, myMove as Move2) +
-    //     movePoint(myMove as Move2)
-    // )
-    // .reduce((acc, current) => acc + current, 0);
+    const answer = data
+      .split("\n")
+      .map(line => line.split(""))
+      .map(line => {
+        const left = line.slice(0, line.length / 2);
+        const right = line.slice(line.length / 2, line.length);
+        let repeated;
+        for (let letterIndex = 0; letterIndex < line.length; letterIndex++) {
+          if (
+            left.includes(line[letterIndex]) &&
+            right.includes(line[letterIndex])
+          ) {
+            repeated = line[letterIndex];
+            break;
+          }
+        }
+        return repeated;
+      });
+
     console.log(answer);
   } catch (err) {
     console.error(err);
