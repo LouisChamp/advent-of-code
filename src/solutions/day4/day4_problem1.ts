ee6a4e40e557116cf16108bdad3c1ec72b744225
Luiz Rocco <luizgustavorocco@gmail.com>
2022-12-11 15:22:54 -0300

    day 4 start

--- src/solutions/day4/day4_problem1.ts
+++ src/solutions/day4/day4_problem1.ts
@@ -5,8 +5,15 @@ export const day4_problem1 = () => {
   try {
     const problemPath = path.join(__dirname, "problem1_input.txt");
     const data: string = fs.readFileSync(problemPath, "utf8");
-    const answer = data.split("\n");
-
+    const answer = data
+      .split("\n")
+      .map(line => {
+        return line.split(",").map(range => range.split("-").map(Number));
+      })
+      .map(line => {
+        const a = line.map(([start, end]) => end - start);
+        const max = Math.max(...a);
+      });
     console.log(answer);
   } catch (err) {
     console.log(err);
