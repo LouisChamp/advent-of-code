9af6fa1d3ecb062309c47956c01953c6d53a9260
Luiz Rocco <luizgustavorocco@gmail.com>
2022-12-12 00:46:39 -0300

    Day 5 start

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import {
   day3_problem2,
   day4_problem1,
   day4_problem2,
+  day5_problem1,
 } from "./solutions";
 
 const main = (mode: "all" | "last"): void => {
@@ -17,6 +18,7 @@ const main = (mode: "all" | "last"): void => {
     day3_problem2,
     day4_problem1,
     day4_problem2,
+    day5_problem1,
   ];
 
   if (mode === "all") {
